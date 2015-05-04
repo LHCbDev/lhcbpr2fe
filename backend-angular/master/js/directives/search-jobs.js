3075b5526a40e84ac573bc05cbed0add76050ffd
Amine Ben hammou <webneat@gmail.com>
2015-05-04 14:03:31 +0200

    search-jobs directive changed to be able to filter only options, or only versions or both

--- backend-angular/master/js/directives/search-jobs.js
+++ backend-angular/master/js/directives/search-jobs.js
@@ -6,48 +6,56 @@
 App.directive('searchJobs', ["lhcbprResources", function(lhcbprResources){
 	return {
 		templateUrl: 'app/views/directives/search-jobs.html',
-    	scope: {onFound: '&'},
+    	scope: {
+    		onFound: '&',
+    		filterOptions: '@',
+    		filterVersions: '@',
+    		filterJobs: '@'
+    	},
 		link: function(scope, element, attrs) {
 			scope.versionsIds = [];
 			scope.applicationIds = [];
-			scope.optionsIds = []
-
-			lhcbprResources.all("active/applications").getList().then(
-				function(apps){
-					scope.apps = apps;
-				}
-			);
+			scope.optionsIds = [];
 
 			scope.$watch("app", function(newApp) {
 				if (newApp) {
-					scope.applicationIds = [newApp.id]
-					lhcbprResources.all(
-						"active/applications/" + newApp.id +'/versions/'
-					).getList().then(
-						function(versions){
-							cleanVersionsIds();	
-							scope.versions = versions;
-							scope.versionChanged();
-						}
-					);
+					scope.applicationIds = [newApp.id];
+					if(scope.filterVersions !== 'false'){
+						lhcbprResources.all(
+							"active/applications/" + newApp.id +'/versions/'
+						).getList().then(
+							function(versions){
+								cleanVersionsIds();	
+								scope.versions = versions;
+								scope.done = ! scope.done;
+								searchJobs();
+							}
+						);
+					}
+					if(scope.filterOptions !== 'false'){
+						lhcbprResources.all("active/applications/" + scope.app.id + "/options").getList(
+							{versions: scope.versionsIds.join()}
+						).then(function(options){
+							scope.options = options;
+							scope.allOptionsIds = getAllIds(options);
+							// console.log(scope.allOptionsIds, scope.optionsIds);
+							scope.optionsIds = scope.optionsIds.filter(function(el){
+								return scope.allOptionsIds.indexOf(el) != -1;
+							});
+							scope.done = ! scope.done;
+							searchJobs();
+						});
+					}
 				}
 			});
 
 			scope.versionChanged = function(){
-				lhcbprResources.all("active/applications/" + scope.app.id + "/options").getList(
-					{versions: scope.versionsIds.join()}
-				).then(function(options){
-					scope.options = options;
-					scope.allOptionsIds = getAllIds(options);
-					console.log(scope.allOptionsIds, scope.optionsIds);
-					scope.optionsIds = scope.optionsIds.filter(function(el){
-						return scope.allOptionsIds.indexOf(el) != -1;
-					});
-					searchJobs();
-				});
+				scope.done = true;
+				searchJobs();
 			};
 
 			scope.optionChanged = function() {
+				scope.done = true;
 				searchJobs();
 			};
 
@@ -60,22 +68,31 @@ App.directive('searchJobs', ["lhcbprResources", function(lhcbprResources){
 			};
 
 			var searchJobs = function() {
-				lhcbprResources.all("search-jobs").getList(
-					{
-						application: scope.applicationIds.join(),
-						versions: scope.versionsIds.join(),
-						options: scope.optionsIds.join()
-					}
-					).then(function(jobs){
-					scope.onFound({
-						apps: scope.applicationIds,
-						options: scope.optionsIds,
-						versions: scope.versionsIds,
-						jobs:jobs
+				if(scope.done){
+					lhcbprResources.all("search-jobs").getList(
+						{
+							application: scope.applicationIds.join(),
+							versions: scope.versionsIds.join(),
+							options: scope.optionsIds.join()
+						}
+						).then(function(jobs){
+						scope.onFound({
+							apps: scope.applicationIds,
+							options: scope.optionsIds,
+							versions: scope.versionsIds,
+							jobs:jobs
+						});
 					});
-				});
+				}
 			};
-			searchJobs();
+
+			lhcbprResources.all("active/applications").getList().then(
+				function(apps){
+					scope.apps = apps;
+					scope.done = true;
+					searchJobs();
+				}
+			);
 		}
 	}
 }]);
--- backend-angular/master/modules/statistics/js/controllers/statistics.js
+++ backend-angular/master/modules/statistics/js/controllers/statistics.js
@@ -1,11 +1,8 @@
 App.controller('StatisticsController', ['$scope', function ($scope) {
 	
-	$scope.requestStatistics = function (apps, options, versions, jobs){
-		console.log('requestStatistics called !');
-		console.log('app: ', app); 
+	$scope.requestStatistics = function (apps, options){
+		console.log('apps: ', apps); 
 		console.log('options: ', options); 
-		console.log('versions: ', versions); 
-		console.log('jobs: ', jobs);
 	};
 
 }]);
