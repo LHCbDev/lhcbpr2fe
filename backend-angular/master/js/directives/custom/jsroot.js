3908749946e08a296a779779257a54ed976fc76c
Alexander Mazurov <alexander.mazurov@gmail.com>
2016-09-06 14:52:17 +0200

    Remove  console.log

--- backend-angular/master/js/directives/custom/jsroot.js
+++ backend-angular/master/js/directives/custom/jsroot.js
@@ -289,7 +289,6 @@ App.directive('rootjsserver', function($http) {
       	scope.data = others[0];
 
       }else{
-      	console.log("SASHA", graphs);
       	scope.data = JSROOT.CreateTMultiGraph.apply(this, graphs);
       }
     }
