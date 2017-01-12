dbbf5db0c4a684a8dad22cd68bbb424b1c273ba7
Dmitry Popov <Dmitry.Popov@cern.ch>
2017-01-12 13:33:23 +0100

    Added detailed timing code

--- backend-angular/master/modules/validations/js/init.js
+++ backend-angular/master/modules/validations/js/init.js
@@ -1,15 +1,28 @@
 Module.create('validations', 'Gauss', 2)
-  .addMenuItems({
+  .addMenuItems([{
     text: "Validation analysis",
     sref: "app.validations",
     icon: "icon-grid"
-  })
+  },
+  .addMenuItems([{
+    text: "Detailed timing",
+    sref: "app.detime",
+    icon: "icon-grid"
+  },
+  ])
   .addStates({
     name: 'validations',
     title: 'Validation',
     url: '/validations',
     templateUrl: 'validations.html',
     resolve: ['validations', 'chartjs', 'ngTable', 'ngDialog', 'jsroot']
+  })
+  .addState({
+    name: 'detime',
+    title: 'Detailed timing',
+    url: '/detime',
+    templateUrl: 'detime.html',
+    resolve: ['validations', 'jsroot', 'ngTable', 'ngDialog']
   })
   .start();
 
