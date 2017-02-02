89b2ffce1247472635d31d74d87f9867cb11d233
Nathanael Farley <nathanael.a.s.farley@cern.ch>
2017-02-02 21:17:45 +0100

    Improved sortFileContentsToJSON function.

--- backend-angular/master/js/classes/module.js
+++ backend-angular/master/js/classes/module.js
@@ -57,7 +57,8 @@ var ModuleHelpers = new function() {
          $scope.plotViews = [];
          $scope.plotViewsFromProvider = plotViewsFromProvider;
          if(undefined !== plotViewsFromArgs) {
-           for(let i in plotViewsFromProvider) {
+           let i;
+           for(i in plotViewsFromProvider) {
              let index = _.findIndex(
                plotViewsFromArgs,
                function(o) {return o === plotViewsFromProvider[i].directiveName;});
@@ -69,6 +70,13 @@ var ModuleHelpers = new function() {
            $scope.plotViews = plotViewsFromProvider;
          }
          // TODO check for plotViews requested but not found
+
+         $scope.selectionLevels = {};
+         $scope.addSelectionLevel = function(currentLevel, newLevelData) {
+           // TODO remove lower levels
+           $scope.data.treedirsStructure
+           $scope.selectionLevels[currentLevel] = newLevelData;
+         };
 
          $scope.selectedApp = restrict.selectedApp;
          $scope.selectedOptions = restrict.selectedOptions;
@@ -106,23 +114,12 @@ var ModuleHelpers = new function() {
          $scope.panelJobs = {toggle: false};
          $scope.hidePanelDebug = true;
 
-         $scope.sizeOf = function(obj) {
-           return Object.keys(obj).length;
-         };
+         // $scope.sizeOf = function(obj) {
+         //   return Object.keys(obj).length;
+         // };
 
          $scope.showSearchForm = function() {
            $scope.isShowSearchForm = true;
-         };
-
-         $scope.getJobName = function(id) {
-           var job = $scope.cachedJobs[id];
-           if (job) {
-             var av = job.job_description.application_version;
-             var opt = job.job_description.option;
-             return 'Job ID ' + job.id + ': ' + av.application.name + ' ' + av.version + ' - ' + job.platform.content + ' - ' + opt.description;
-           } else {
-             return 'undefined';
-           }
          };
 
          $scope.lookHistos = function(jids) {
@@ -160,6 +157,7 @@ var ModuleHelpers = new function() {
                .then(function(attr) { // When we receive the response
                  var res = [];
                  for (i = 0; i < attr.length; i++) {
+                   let j;
                    for (j = 0; j < attr[i].jobvalues.length; j++) {
                      if ( attr[i].jobvalues[j].value.endsWith(".root") ) {
                        var file = attr[i].jobvalues[j].job.id + '/' + attr[i].jobvalues[j].value;
@@ -183,6 +181,7 @@ var ModuleHelpers = new function() {
          };
 
          $scope.readFiles = function () {
+           $scope.noJobData = ($scope.jobId.length < 1);
            if ($scope.jobId && $scope.jobId.length > 0) {
              var parameters = {
                files: $scope.jobId,
@@ -197,71 +196,11 @@ var ModuleHelpers = new function() {
                }
 
                $scope.data.treedirs = retVal;
+               $scope.data.treedirsStructure = $apiroot.sortFileContentsToJSON(response);
              });
            };
          };
 
-         $scope.readTree = function() {
-           /**
-            * Reads tree using LHCbPR2ROOT. Adds dir to $scope.data.treedirs
-            */
-           if ($scope.jobId && $scope.jobId.length > 0) {
-             var parameters = {
-               files: $scope.jobId,
-               folders: $scope.folders
-             };
-             $apiroot.lookupDirs(parameters).then(function (response) {
-               $scope.noJobData = (response.length < 1);
-               $scope.data.tree = response;
-               listfn = Object.keys(response);
-               var intersect = Object.keys(response[ listfn[0] ]['/']);
-               for ( key = 1; key < listfn.length; key++ ) {
-                 var list = Object.keys(response[ listfn[key] ]['/']);
-                 // intersect becomes inner join of intersect and list
-                 intersect = $(list).filter(intersect);
-               }
-               $scope.data.treedirs[listfn.join(',')] = {};
-               for ( key = 0; key < intersect.length; key++ ) {
-                 namecat = intersect[key].replace(/List_*/, "/").replace("__","/");
-                 $scope.data.treedirs[listfn.join(',')][intersect[key]] = namecat;
-               }
-             });
-           } else {
-             $scope.noJobData = ($scope.jobId.length < 1);
-           }
-         };
-
-         // $scope.showPlots = function(file, namecat) {
-         //   if ( $scope.data.plotSelect != null ) $scope.data.plotSelect[file] = "";
-         //   var listfn = file.split(',');
-         //   var intersect = $scope.data.tree[listfn[0]]['/'][namecat];
-
-         //   for ( key = 1; key < listfn.length; key++ ) {
-         //     var keys = {};
-         //     for (var i in intersect)
-         //       if (i in $scope.data.tree[listfn[key]]['/'][namecat])
-         //         keys[i] = $scope.data.tree[listfn[key]]['/'][namecat][i];
-         //     intersect = keys;
-         //   }
-         //   $scope.data.treeplots[file] = intersect;
-         // };
-
-         // $scope.showChart = function(file, title) {
-         //   files={}
-         //   var listfn = file.split(',');
-         //   for ( key in listfn ) {
-         //     files[listfn[key]] = 'Job ID: ' + listfn[key].split('/')[0];
-         //   }
-         //   titles={}
-         //   titles[title] = $scope.data.treeplots[file][title];
-         //   $scope.url = BUILD_PARAMS.url_root;
-         //   $scope.files_and_titles = files;
-         //   if ( title == "ALL" )
-         //     $scope.data.graphs = $scope.data.treeplots[file];
-         //   else
-         //     $scope.data.graphs = titles;
-
-         // };
        }]);
   };
 };
--- backend-angular/master/js/services/root-resources.js
+++ backend-angular/master/js/services/root-resources.js
@@ -52,21 +52,84 @@ App.service('rootResources', ['$http', 'BUILD_PARAMS', function($http, BUILD_PAR
      * If it does not, this is a bug!
      */
 
-    // Taken straight out of SO:
-    // https://stackoverflow.com/questions/36248245/how-to-convert-an-array-of-paths-into-json-structure
     var parsePathArray = function(paths) {
-      var parsed = {};
-      for(var i = 0; i < paths.length; i++) {
-        var position = parsed;
-        var split = paths[i].split('/');
-        for(var j = 0; j < split.length; j++) {
-          if(split[j] !== "") {
-            if(typeof position[split[j]] === 'undefined')
-              position[split[j]] = {};
-            position = position[split[j]];
+      var parsed = {
+        name: "/",
+        location: "/",
+        children: {}
+      };
+      var i;
+      var j;
+      for(i in paths) {
+        var path = paths[i];
+        var splitPath = path.split('/');
+
+        // Remove falsy values like "" from splitPath
+        splitPath = _.remove(splitPath);
+
+        var partialParsedPath = ["children"];
+
+        for(j in splitPath) {
+          var objName = splitPath[j];
+          // Check if the obj is a directory
+          // If this parsedPath does not exist, create it
+          // TODO tidy up
+          var existingObjs = _.filter(
+            _.get(parsed, partialParsedPath),
+            function(val, key, coll) {
+              return typeof val === "object" && val.name === objName;
+            });
+
+          if(existingObjs.length === 1) {
+            // Find it, add it
+            // partialParsedPath.push(_.indexOf(_.get(parsed, partialParsedPath), existingObjs[0]));
+            partialParsedPath.push(objName);
+            if(_.includes(path, "/"+objName+"/")) {
+              partialParsedPath.push("children");
+            };
+          } else if(existingObjs.length > 1) {
+            console.error("Something has gone wrong!");
+            debugger;
+          } else {
+            // partialParsedPath.push(_.get(parsed, partialParsedPath).length);
+            partialParsedPath.push(objName);
+            _.set(parsed, partialParsedPath, {
+              name: objName,
+              location: "/"+_.slice(splitPath, 0, Number(j)+1).join('/') // If you pass this location to jsroot, it will fetch the object with objName
+            });
+            // If it's a directory, give it children.
+            if(_.includes(path, "/"+objName+"/")) {
+              partialParsedPath.push("children");
+              _.set(parsed, partialParsedPath, {});
+            };
           }
-        }
-      }
+          // So, it does exist. What now?
+          //
+          // If it's a directory, then this loop will continue and needs a
+          // reference to the last element in children of this object. If not,
+          // do nothing and the loop should discontinue
+
+          // if(_.includes(path, "/"+objName+"/")) {
+          //   // TODO assert that partialParsedPath ends in children at this point!
+
+          //   // Add a reference to the last+1th element.
+          //   // debugger;
+          //   partialParsedPath.push(""+_.get(parsed, partialParsedPath).length);
+          // };
+        };
+      };
+      // var parsed = {};
+      // for(var i = 0; i < paths.length; i++) {
+      //   var position = parsed;
+      //   var split = paths[i].split('/');
+      //   for(var j = 0; j < split.length; j++) {
+      //     if(split[j] !== "") {
+      //       if(typeof position[split[j]] === 'undefined')
+      //         position[split[j]] = {};
+      //       position = position[split[j]];
+      //     }
+      //   }
+      // }
       return parsed;
     };
 
@@ -75,6 +138,8 @@ App.service('rootResources', ['$http', 'BUILD_PARAMS', function($http, BUILD_PAR
     _.forEach(filesJSON, function(value, key) {
       retObj[key] = parsePathArray(value);
     });
+
+    console.debug(JSON.stringify(retObj, null, 2));
 
     return retObj;
   }
