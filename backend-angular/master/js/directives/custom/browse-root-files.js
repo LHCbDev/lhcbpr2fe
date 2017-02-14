2881b6c88fe7242bdc9e4bf70466e1d75ee10101
Nathanael Farley <nathanael.a.s.farley@cern.ch>
2017-02-14 21:51:40 +0100

    Tidied browse-root-files directive code.

--- backend-angular/master/js/directives/custom/browse-root-files.js
+++ backend-angular/master/js/directives/custom/browse-root-files.js
@@ -17,7 +17,50 @@ App.directive('browseRootFiles', [function () {
       function($scope, $q, rootResources, resourceParser) {
         var that = this;
 
-        this.sortFileContentsToJSON = function(resources, filesJSON) {
+
+        // TODO find a better name for this function
+        var getPathUpToLevel = function(path, level) {
+          /**
+           * Takes a path and a level and returns the path up to that level.
+           *
+           * NOTE: the 0th level is the first level.
+           *
+           * e.g.
+           * getLocationFromLevel("/foo/bar/baz", 1); => "/foo/bar"
+           * 
+           */
+          var splitPath = path.split('/');
+          // Remove falsy values like "" from splitPath
+          splitPath = _.remove(splitPath);
+
+          return "/"+_.slice(splitPath, 0, Number(level)+1).join('/');
+        };
+
+
+        var createPayload = function(resources, locationInFile) {
+          return angular.copy({
+            locationInFile: locationInFile,
+            resources: angular.copy(resources)
+          });
+        };
+
+        var createMenuNode = function(name, fileName, hasChildren, payload) {
+          var retObj = {
+            name: name,
+            fileName: fileName,
+            isExpanded: false
+          };
+          if(hasChildren) {
+            retObj.children = [];
+          }
+          if(payload) {
+            retObj.payload = payload;
+          }
+          return retObj;
+        };
+
+
+        this.sortFileContentsToJSON = function(resources, filesObj, regexes) {
           /**
            * This is designed to take the output of this.lookupFileContents and output
            * it into something more reasonable to navigate in javascript.
@@ -25,67 +68,16 @@ App.directive('browseRootFiles', [function () {
            * If it does not, this is a bug!
            */
 
-          // TODO find a better name for this function
-          var getPathUpToLevel = function(path, level) {
-            /**
-             * Takes a path and a level and returns the path up to that level.
-             *
-             * NOTE: the 0th level is the first level.
-             *
-             * e.g.
-             * getLocationFromLevel("/foo/bar/baz", 1); => "/foo/bar"
-             * 
-             */
-            var splitPath = path.split('/');
-            // Remove falsy values like "" from splitPath
-            splitPath = _.remove(splitPath);
 
-            return "/"+_.slice(splitPath, 0, Number(level)+1).join('/');
-          };
-
-          var createPayload = function(resources, locationInFile) {
-            return angular.copy({
-              locationInFile: locationInFile,
-              resources: angular.copy(resources)
-            });
-            // return locationInFile;
-          };
-
-          // var findResourceWithName = function(resources, name) {
-          //     var i;
-          //     for(i in resources) {
-          //         if(resourceParser.getCommonValue(resources[i]) === name) {
-          //             return angular.copy(resources[i]);
-          //         }
-          //     }
-          //     console.error(name+" not found in resources:");
-          //     console.error(JSON.stringify(resources, null, 2));
-          //     return undefined;
-          // };
-
-          var parsePathArray = function(files) {
-            // var parsed = {
-            //   name: "/",
-            //   location: "/",
-            //   children: []
-            // };
             var parsed = [];
             var i;
             var j;
             var fileName;
-            for(fileName in files){
-              var paths = files[fileName];
-              // var resource = resourceParser.findResourceWithCommonValue(resources, fileName);
+            for(fileName in filesObj){
+              var paths = filesObj[fileName];
               var resourcesMatchingRegExp = resourceParser.findResourcesWithRegexValue(
                 resources, new RegExp("^"+fileName+"$"));
-              // var partialParsedPath = [parsed.length];
-              parsed.push({
-                name: fileName,
-                fileName: fileName,
-                children: []
-              });
-
-              // partialParsedPath.push("children");
+              parsed.push(createMenuNode(fileName, fileName, true));
 
               for(i in paths) {
                 var partialParsedPath = [parsed.length-1, "children"];
@@ -99,7 +91,6 @@ App.directive('browseRootFiles', [function () {
                   var objName = splitPath[j];
                   // Check if the obj is a directory
                   // If this parsedPath does not exist, create it
-                  // TODO tidy up
                   var existingObjs = _.filter(
                     _.get(parsed, partialParsedPath),
                     function(val, key, coll) {
@@ -109,7 +100,6 @@ App.directive('browseRootFiles', [function () {
                   if(existingObjs.length === 1) {
                     // Find it, add it
                     partialParsedPath.push(_.indexOf(_.get(parsed, partialParsedPath), existingObjs[0]));
-                    // partialParsedPath.push(objName);
                     if(_.includes(path, "/"+objName+"/")) {
                       partialParsedPath.push("children");
                     };
@@ -117,40 +107,21 @@ App.directive('browseRootFiles', [function () {
                     console.error("Something has gone wrong!");
                   } else {
                     partialParsedPath.push(_.get(parsed, partialParsedPath).length);
-                    // partialParsedPath.push(objName);
-                    _.set(parsed, partialParsedPath, {
-                      name: objName,
-                      // If you pass this payload to
-                      // plot-view-generator, it will fetch
-                      // the object
-                      payload: createPayload(
+                    var isDirectory = _.includes(path, "/"+objName+"/");
+                    _.set(parsed, partialParsedPath, createMenuNode(
+                      objName,
+                      fileName,
+                      isDirectory,
+                      createPayload(
                         resourcesMatchingRegExp,
                         getPathUpToLevel(path, j)
-                      ),
-                      isExpanded: false
-                    });
-                    // If it's a directory, give it children.
-                    if(_.includes(path, "/"+objName+"/")) {
-                      partialParsedPath.push("children");
-                      _.set(parsed, partialParsedPath, []);
-                    };
+                      )
+                    ));
                   }
                 };
               }};
-            return parsed;
-          };
 
-          // var retObj = {};
-
-          // _.forEach(filesJSON, function(value, key) {
-          //   retObj[key] = parsePathArray(value);
-          // });
-
-          // console.debug(JSON.stringify(retObj, null, 2));
-
-          var retObj = parsePathArray(filesJSON);
-
-          return retObj;
+          return parsed;
         };
 
 
@@ -178,7 +149,10 @@ App.directive('browseRootFiles', [function () {
               var strippedKey = key.replace(regex, '');
               newResponse[strippedKey] = response[key];
             }
-            $scope.folders = that.sortFileContentsToJSON($scope.resources, newResponse);
+            var regexes = _.map(Object.keys(newResponse), function(v) { return new RegExp("^"+v+"$"); });
+            // By this point newResponse is an object with keys of filenames,
+            // and values of a list of objects in the file.
+            $scope.folders = that.sortFileContentsToJSON($scope.resources, newResponse, regexes);
           });
         });
 
