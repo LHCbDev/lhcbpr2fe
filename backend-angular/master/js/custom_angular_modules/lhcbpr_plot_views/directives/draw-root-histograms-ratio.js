606731d591f77b16f330748df5f5fb11e729c347
Nathanael Farley <nathanael.a.s.farley@cern.ch>
2017-02-28 11:44:39 +0100

    Modularised the ratio of histograms in generic analysis module.

--- backend-angular/master/js/custom_angular_modules/lhcbpr_plot_views/directives/draw-root-histograms-ratio.js
+++ backend-angular/master/js/custom_angular_modules/lhcbpr_plot_views/directives/draw-root-histograms-ratio.js
@@ -9,8 +9,25 @@ lhcbprPlotModule.directive('drawRootHistogramsRatio', function() {
     // TODO change if needed
     templateUrl: 'app/views/custom_angular_modules/lhcbpr_plot_views/drawRootObject.html',
     controllerAs: "ctrl",
-    controller: ['BUILD_PARAMS', '$scope', function(BUILD_PARAMS, $scope) {
+    controller: ['BUILD_PARAMS', '$scope', 'rootObjManipulator',
+                 function(BUILD_PARAMS, $scope, rootObjManipulator) {
       $scope.BUILD_PARAMS = BUILD_PARAMS;
+
+      $scope.draw = function(pad, objectsToPlot) {
+        var i;
+        var plotColor = false;
+        JSROOT.OpenFile("/api/media/jobs/"+$scope.objectsToPlot()[0].fileLocation, function(file0) {
+          JSROOT.OpenFile("/api/media/jobs/"+$scope.objectsToPlot()[1].fileLocation, function(file1) {
+            file0.ReadObject($scope.objectsToPlot()[0].objectLocation, function(obj0) {
+              file1.ReadObject($scope.objectsToPlot()[1].objectLocation, function(obj1) {
+                var histogram = rootObjManipulator.ratioOfHists(obj0, obj1);
+                // Errors are now invalid, do not plot them.
+                JSROOT.draw(pad, histogram, "HIST");
+              });
+            });
+          });
+        });
+      };
     }],
     link: function(scope, element) {
       // Check that it can be done:
@@ -20,30 +37,9 @@ lhcbprPlotModule.directive('drawRootHistogramsRatio', function() {
       }
       var pad = element.children()[0];
       pad.innerHTML = "";
-      var i;
-      var plotColor = false;
-      JSROOT.OpenFile("/api/media/jobs/"+scope.objectsToPlot()[0].fileLocation, function(file0) {
-        JSROOT.OpenFile("/api/media/jobs/"+scope.objectsToPlot()[1].fileLocation, function(file1) {
-          file0.ReadObject(scope.objectsToPlot()[0].objectLocation, function(obj0) {
-            file1.ReadObject(scope.objectsToPlot()[1].objectLocation, function(obj1) {
-              /// We can't copy the object, so we mutate the existing one.
-              obj0 = JSROOT.JSONR_unref(obj0);
-              _.map(obj0.fArray, function(value, ind) {
-                if(obj1.fArray[ind] === 0) {
-                  obj0.fArray[ind] = 0;
-                } else {
-                  obj0.fArray[ind] = obj0.fArray[ind] / obj1.fArray[ind];
-                }
-              });
 
-              obj0.fTitle = obj0.fTitle + " (Ratio)";
-
-              // Errors are now invalid, do not plot them.
-              JSROOT.draw(pad, obj0, "HIST");
-            });
-          });
-        });
-      });
+      // When this directive is compiled/rendered, plot the plots.
+      scope.draw(pad, scope.objectsToPlot);
     }
   };
 });
