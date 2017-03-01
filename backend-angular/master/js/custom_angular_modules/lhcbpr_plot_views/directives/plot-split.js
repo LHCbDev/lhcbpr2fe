665392c978b4511de74a8659466ef5cd6dd1e723
Nathanael Farley <nathanael.a.s.farley@cern.ch>
2017-03-01 18:39:24 +0100

    Replaced table with bootstrap rows and columsn in plotSplit.

--- backend-angular/master/js/custom_angular_modules/lhcbpr_plot_views/directives/plot-split.js
+++ backend-angular/master/js/custom_angular_modules/lhcbpr_plot_views/directives/plot-split.js
@@ -1,1 +1,28 @@
-plotDirectiveFactory('plotSplit', 'Split');
+lhcbprPlotModule.directive("plotSplit", function() {
+  return {
+    restrict: 'E',
+    scope: {
+      resources: '=',
+      graphs: '=',
+      files: '=',
+      test: '=',
+      url: '='
+    },
+    // TODO make this a less magic folder path, possibly by adding a method to
+    // the lhcbprPlotModule or something
+    templateUrl: 'app/views/custom_angular_modules/lhcbpr_plot_views/plotSplit.html',
+    controllerAs: "ctrl",
+    controller: ['$scope', 'resourceParser', function($scope, resourceParser) {
+
+      $scope.setColClass = function(numGraphs) {
+        debugger;
+        return "col-md-"+Math.floor(12/numGraphs);
+      };
+
+      this.getFilesFromResources = resourceParser.getFilesFromResources;
+    }]
+  };
+});
+lhcbprPlotModule.config(['plotViewsProvider', function(plotViewsProvider) {
+  plotViewsProvider.registerPlotView('plotSplit', 'Split');
+}]);
