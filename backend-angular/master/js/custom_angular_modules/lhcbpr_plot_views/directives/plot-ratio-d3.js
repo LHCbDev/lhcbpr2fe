d3fd051d9e2dcdaa3354245419289fa092a5a3ad
Nathanael Farley <nathanael.a.s.farley@cern.ch>
2017-04-27 16:50:06 +0200

    Cleaned up code in other plot* directives.

--- backend-angular/master/js/custom_angular_modules/lhcbpr_plot_views/directives/plot-ratio-d3.js
+++ backend-angular/master/js/custom_angular_modules/lhcbpr_plot_views/directives/plot-ratio-d3.js
@@ -1,18 +1,13 @@
-lhcbprPlotModule.directive("plotRatioD3", function() {
+lhcbprPlotModule.directive("plotRatioD3", ["defaultTemplateDir", function(defaultTemplateDir) {
   return {
     restrict: 'E',
     scope: {
       graphs: '&',
       url: '='
     },
-    // TODO make this a less magic folder path, possibly by adding a method to
-    // the lhcbprPlotModule or something
-    templateUrl: 'app/views/custom_angular_modules/lhcbpr_plot_views/plot-ratio-d3.html',
+    templateUrl: defaultTemplateDir+'plot-ratio-d3.html',
     controllerAs: "ctrl",
     controller: ['$scope', 'resourceParser', function($scope, resourceParser) {
-      $scope.binEdges = [1, 2, 3, 4];
-      $scope.binValues = [1, 2, 3];
-
       this.getObjectsToPlot = function(payload) {
         // Returns a map of {fileLocation: xxx, objectLocation: xxx} for items
         var fileNames = resourceParser.getFilesFromResources(payload.resources);
@@ -26,7 +21,7 @@ lhcbprPlotModule.directive("plotRatioD3", function() {
       };
     }]
   };
-});
+}]);
 lhcbprPlotModule.config(['plotViewsProvider', function(plotViewsProvider) {
   plotViewsProvider.registerPlotView("plotRatioD3", "Ratio (with d3)");
 }]);
